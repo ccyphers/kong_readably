1b5cfd1b9a219fc7a17e2ff887dadf1396de749c
Cliff Cyphers <ccyphers@Cliffs-MacBook-Pro.local>
2017-02-12 11:29:00 -0600

    correct invalid schema

--- kong-readably/schema.lua
+++ kong-readably/schema.lua
@@ -1,5 +1,6 @@
 return {
   no_consumer = true, -- this plugin will only be API-wide,
+  fields = {},
   self_check = function(schema, plugin_t, dao, is_updating)
     -- perform any custom verification
     return true
